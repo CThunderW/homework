6ec1594fa10cf40eda6e283f50a618b02916b5f8
Cale Williams <calethunderwilliams@gmail.com>
2018-08-05 18:13:41 -0700

    starter functions added

--- boxit/boxIt.js
+++ boxit/boxIt.js
@@ -1,2 +1,28 @@
-function drawLine(){}
+
+function drawLine(inputName){
+   return "-".repeat(inputName.length);
+}
+
+function drawTopBorder(inputName){
+    return `┏${drawLine(inputName)}"┓`;
+};
+
+function drawMiddleBorder(inputName){
+    return `┣${drawLine(inputName)}┫`;
+};
+
+function drawBottomBorder(inputName){
+    return `┗${drawLine(inputName)}┛`;
+};
+
+
+function drawBarsAround(inputName){
+    return `|${inputName}|`;
+};
+
+drawTopBorder(inputName)
+drawBarsAround(inputName)
+drawMiddleBorder(inputName)
+
+function boxIt(inputName, ...) {}
 
